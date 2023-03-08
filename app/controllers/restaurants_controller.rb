323e772ee0430a4ecc0bed116b43136860778b6d
Leanne Tay <duckingville@yahoo.com>
2023-03-08 15:09:22 +0800

    form creates new valid restaurant when subnitted

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurants, only: %i[index]
   before_action :set_restaurant, only: %i[show]
+  before_action :new_restaurant, only: %i[new, create]
 
   def index
     @restaurants
@@ -11,7 +12,17 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant
+  end
+
+  def create
     @restaurant = Restaurant.new
+    restaurant = Restaurant.new(restaurant_params)
+    if restaurant.save
+      redirect_to restaurant_path(restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -25,5 +36,13 @@ class RestaurantsController < ApplicationController
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def new_restaurant
+    @restaurant = Restaurant.new
   end
 end
