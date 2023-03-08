22db5562733ca643efcf543844d05558053e1af0
Leanne Tay <duckingville@yahoo.com>
2023-03-08 14:41:20 +0800

    added appropriate CRUD routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'restaurant/index'
-  get 'restaurant/show'
-  get 'restaurant/new'
-  get 'restaurant/edit'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants do
+    resources :reviews, only: [:index, :new]
+  end
 end
