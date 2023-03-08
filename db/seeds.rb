aa48cdfde0a3c013132f12be18c41250c482ebf2
Leanne Tay <duckingville@yahoo.com>
2023-03-08 14:33:12 +0800

    seeded restaurant table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create!(name: 'Restaurant 1', address: '1 restaurant road', phone_number: '1231231', category: 'chinese')
+Restaurant.create!(name: 'Restaurant 2', address: '2 restaurant road', phone_number: '1231232', category: 'italian')
+Restaurant.create!(name: 'Restaurant 3', address: '3 restaurant road', phone_number: '1231233', category: 'japanese')
+Restaurant.create!(name: 'Restaurant 4', address: '4 restaurant road', phone_number: '1231234', category: 'french')
+Restaurant.create!(name: 'Restaurant 5', address: '5 restaurant road', phone_number: '1231235', category: 'belgian')
