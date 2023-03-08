69a371b342011750917000f64c3d8ec65499487f
Leanne Tay <duckingville@yahoo.com>
2023-03-08 15:57:24 +0800

    added page for new reviews to be submitted

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,27 @@
 class ReviewsController < ApplicationController
-  def show
+  before_action :set_restaurant
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def set_reviews
-    @reviews = Review.all
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
-  def set_review
-    @review = Review.find(params[:id])
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
